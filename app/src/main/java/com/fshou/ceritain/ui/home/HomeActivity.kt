b24699d5d679b76029dfb71a28d3ed99390d34ee
Faqih Suyudi <faqihsuyudi2020@gmail.com>
2024-03-28 11:30:51 +0800

    initial page onboarding

--- app/src/main/java/com/fshou/ceritain/ui/home/HomeActivity.kt
+++ app/src/main/java/com/fshou/ceritain/ui/home/HomeActivity.kt
@@ -1,11 +1,13 @@
 package com.fshou.ceritain.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.fshou.ceritain.R
+import com.fshou.ceritain.ui.onboarding.OnBoardingActivity
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,5 +19,7 @@ class HomeActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        startActivity(Intent(this,OnBoardingActivity::class.java))
+        finish()
     }
 }
