c403600a9ae078da7a579a543ec0f27eee03d52f
Faqih Suyudi <faqihsuyudi2020@gmail.com>
2024-03-28 08:40:06 +0800

    add logo, initial pages

---
+++ app/src/main/java/com/fshou/ceritain/ui/capture/CaptureActivity.kt
@@ -0,0 +1,21 @@
+package com.fshou.ceritain.ui.capture
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.fshou.ceritain.R
+
+class CaptureActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_capture)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
--- app/src/main/java/com/fshou/ceritain/ui/post/PostActivity.kt
+++ app/src/main/java/com/fshou/ceritain/ui/post/PostActivity.kt
@@ -1,16 +1,17 @@
-package com.fshou.ceritain
+package com.fshou.ceritain.ui.post
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.fshou.ceritain.R
 
-class MainActivity : AppCompatActivity() {
+class PostActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_post)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
